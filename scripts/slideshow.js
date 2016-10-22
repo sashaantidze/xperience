3c7b576c9e100326fc06811aed00aa276211e082
sashaantidze <antidze.sasha@gmail.com>
2016-10-22 19:26:00 +0400

    cont-layout

---
+++ scripts/slideshow.js
@@ -0,0 +1,33 @@
+$(function() {
+
+	var width = 960;
+	var animationSpeed = 1300;
+	var pause = 2500;
+	currentSlide = 1;
+
+	var $slider = $('#slider');
+	var $sliderContainer = $slider.find('.slides');
+	var $slides = $sliderContainer.find('.slide');
+
+	var intrval;
+
+	function startSlider(){
+		inerval = setInterval(function(){
+			$sliderContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
+				//currentSlide++;
+				if (++currentSlide === $slides.length){
+					currentSlide = 1;
+					$sliderContainer.css('margin-left', 0);
+				}
+			});
+		}, pause);
+	}
+
+	function pauseSlider(){
+		clearInterval(inerval);
+	}
+
+	$slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
+
+	startSlider();
+});
